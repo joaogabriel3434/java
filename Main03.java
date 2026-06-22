b576df75762e29791ae841773dc513c3a27b400e
joaogabriel3434 <joaogabrieldev34@gmail.com>
2026-06-22 17:57:44 -0300

    Operadores Relacionais

---
+++ Main03.java
@@ -0,0 +1,15 @@
+public class Main03 {
+    public static void main(String[] args) {
+        float resto = 10 %3;
+        System.out.println(resto);
+
+        //<> <= >= == !=//
+        boolean DezMaiorQueVinte = 10 > 20;
+        boolean DezMenorQueVinte = 10 < 20;
+        boolean DezIgualAVinte = 10 == 20;
+
+        System.out.println(DezMaiorQueVinte);
+        System.out.println(DezMenorQueVinte);
+        System.out.println(DezIgualAVinte);
+    }
+}
