a4605ea03369d88fbfae7fa9c04439ef65135a89
joaogabriel3434 <joaogabrieldev34@gmail.com>
2026-06-22 17:05:17 -0300

    Primeiro Exercício em Java

---
+++ Main.java
@@ -0,0 +1,14 @@
+public class Main {
+
+    public static void main(String[] args) {
+        
+        String nome = "João";
+        int idade = 18;
+        float salario = 2500.0f;
+        String data = "05/07/2026";
+
+        String relatorio = "Olá, me chamo " +nome +  ", tenho " + idade + " Anos e recebi meu primeiro salário de " + salario + " no dia " + data;
+        System.out.println(relatorio);
+    }
+    
+}
