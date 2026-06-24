fc70df630ad0736d4bcf10a5b2c591a631b4c441
joaogabriel3434 <joaogabrieldev34@gmail.com>
2026-06-24 15:45:26 -0300

    Exercício com if, else e else if.

---
+++ Main06.java
@@ -0,0 +1,24 @@
+import java.sql.SQLInvalidAuthorizationSpecException;
+
+public class Main06 {
+    public static void main(String[] args) {
+        
+        double SalarioAnual = 70000;
+        double PrimeiraFaixa = 9.70 / 100;
+        double SegundaFaixa = 37.35 / 100;
+        double TerceiraFaixa  = 49.50 / 100;
+        double ValorImposto;
+
+        if(SalarioAnual <=34712){
+            ValorImposto = SalarioAnual * PrimeiraFaixa;
+        }
+        else if(SalarioAnual >= 34713 && SalarioAnual <=60507){
+            ValorImposto = SalarioAnual * SegundaFaixa;
+        }
+        else{
+            ValorImposto = SalarioAnual * TerceiraFaixa;
+        }
+
+        System.out.println("O valor de imposto que você paga por ano é de R$"+ValorImposto);
+    }
+}
