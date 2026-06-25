d3847206aac5c97b4a066b99d425ef9590f34e70
joaogabriel3434 <joaogabrieldev34@gmail.com>
2026-06-25 20:47:21 -0300

    For

--- Main08.java
+++ Main08.java
@@ -5,5 +5,18 @@ public class Main08 {
         while (count <10){
             System.out.println(++count);
         }
+
+
+        for(int i=1; i<10;i++){
+            System.out.println(i);
+        }
+        
+        //Exercício de FOR//
+
+        for(int i = 3; i<100001; i++){
+            if(i % 2 == 0){
+                System.out.println(i);
+            }
+        }
     }
 }
