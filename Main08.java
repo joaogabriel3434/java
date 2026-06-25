5b5c09742cf19391e06e0677d71e6bee1e5a45b6
joaogabriel3434 <joaogabrieldev34@gmail.com>
2026-06-25 18:57:58 -0300

    Estrutura de repetição While

---
+++ Main08.java
@@ -0,0 +1,9 @@
+public class Main08 {
+    public static void main(String[] args) {
+        int count = 0;
+
+        while (count <10){
+            System.out.println(++count);
+        }
+    }
+}
