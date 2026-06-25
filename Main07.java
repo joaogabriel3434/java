04d449f0583f3832356d2602102c9850d3c896e4
joaogabriel3434 <joaogabrieldev34@gmail.com>
2026-06-25 16:45:25 -0300

    Switch

---
+++ Main07.java
@@ -0,0 +1,95 @@
+public class Main07 {
+    public static void main(String[] args) {
+        byte dia = 5;
+        
+        switch (dia) {
+               case 1 : {
+                System.out.println("Domingo");
+            }
+
+               case 2:{
+                System.out.println("Segunda");
+            }
+
+               case 3:{
+                System.out.println("Terça");
+            }
+
+               case 4:{
+                System.out.println("Quarta");
+            }
+
+               case 5:{
+                System.out.println("Quinta");
+                break;
+            }
+
+               case 6:{
+                System.out.println("Sexta");
+            }
+
+               case 7 : {
+                System.out.println("Sábado");
+            }
+            default:
+                System.out.println("Opção Inválida");
+
+            
+        }
+            char sexo = 'M';
+            switch (sexo){
+                case 'M':
+                    System.out.println("Homem");
+                    break;
+
+                case 'F':
+                    System.out.println("Mulher");
+                    break;
+
+                default: 
+                    System.out.println("Opção inválida(Digite apenas M ou N ");
+            }
+            
+            //Atividade Switch//
+
+            int day = 1;
+            switch (day){
+              case 1: 
+                System.out.println("Final de Semana");
+                    break;
+
+                case 2: 
+                    System.out.println("Dia Útil");
+                    break;
+
+                case 3:
+                    System.out.println("Dia Útil");
+                    break;
+                
+                case 4:
+                    System.out.println("Dia Útil");
+                    break;
+
+                case 5:
+                    System.out.println("Dia Útil");
+                    break;
+
+                case 6:
+                    System.out.println("Dia Útil");
+                    break;
+
+                case 7:
+                    System.out.println("Final de semana");
+                    break;
+                
+                default: 
+                    System.out.println("Opção Inválida.");
+            }
+
+
+
+        }
+        }
+            
+    
+   
