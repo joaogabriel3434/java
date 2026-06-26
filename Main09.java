5e58a2e7ce7abc9b4f6582e4241a74aa64a53a45
joaogabriel3434 <joaogabrieldev34@gmail.com>
2026-06-25 21:00:34 -0300

    Break

---
+++ Main09.java
@@ -0,0 +1,14 @@
+public class Main09 {
+    public static void main(String[] args) {
+
+        int ValorMax = 50;
+        for(int i=0; i<=ValorMax; i++){
+            if(i>25){
+                break;
+            }
+            System.out.println(i);
+            
+        }
+        
+    }
+}
