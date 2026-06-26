a90adfa9ad7b79ddeaa381f399a64b183c4e7322
joaogabriel3434 <joaogabrieldev34@gmail.com>
2026-06-26 20:57:24 -0300

    .

--- Main10.java
+++ Main10.java
@@ -1,5 +1,13 @@
 public class Main10 {
     public static void main(String[] args) {
-        
+         double valorTotal = 30000;
+
+        for(int Parcela=1; Parcela<=valorTotal;Parcela++){
+            double valorParcela = valorTotal / Parcela;
+            if (valorParcela<=1000) {
+                continue;
+            }
+            System.out.println("Quantidade de vezes: " +Parcela+ " Valor da parcela em R$: "+valorParcela);
+        }
     }
 }
