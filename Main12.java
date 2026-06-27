127d67b6763e151d29dbcb5e55635b98d7913b1d
joaogabriel3434 <joaogabrieldev34@gmail.com>
2026-06-27 20:10:20 -0300

    Continuação de arrays

---
+++ Main12.java
@@ -0,0 +1,14 @@
+public class Main12 {
+    public static void main(String[] args) {
+        String [] nome = new String[3];
+
+        nome [0] = "Joao";
+        nome [1] = "Stef";
+        nome [2] = "Jonathan";
+
+        for(int i = 0; i<=2; i++){
+            System.out.println(nome [i]);
+            
+        }
+    }
+}
