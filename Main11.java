1cc29ad34bd31df64436302ba672006fe24897ce
joaogabriel3434 <joaogabrieldev34@gmail.com>
2026-06-27 10:58:27 -0300

    Arrays

---
+++ Main11.java
@@ -0,0 +1,14 @@
+public class Main11 {
+    public static void main(String[] args) {
+        //Arrays//
+        int [] idade = new int [3];
+        idade [0] = 21;
+        idade [1] = 20;
+        idade [2] = 30;
+        
+        System.out.println(idade [0]);
+        System.out.println(idade [1]);
+        System.out.println(idade [2]);
+    }
+    
+} 
