7e0ee0fccbd6ceb839b74e5b1a884e51afe08ca9
joaogabriel3434 <joaogabrieldev34@gmail.com>
2026-06-29 20:09:04 -0300

    Continuação de arrays

---
+++ Main13.java
@@ -0,0 +1,9 @@
+public class Main13 {
+    public static void main(String[] args) {
+        int[] numero = new int [3];
+        numero [0] = 1;
+        numero [1] = 2;
+        numero [2] = 3;
+        
+    }
+}
