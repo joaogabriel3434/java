71bf3ac95c71629767c76b33ad5d4992a1c73447
joaogabriel3434 <joaogabrieldev34@gmail.com>
2026-06-30 20:28:55 -0300

    Continuação de estudos

--- Main13.java
+++ Main13.java
@@ -1,9 +1,15 @@
 public class Main13 {
     public static void main(String[] args) {
-        int[] numero = new int [3];
-        numero [0] = 1;
-        numero [1] = 2;
-        numero [2] = 3;
+       int[] numero = new int[]{1,2,3,4,5};
+
+       for(int i=0; i<numero.length;i++){
+            System.out.println(numero[i]);
+       }
+
+       for(int num:numero){
+            System.out.println(num);
+       }
+      
         
     }
 }
---
+++ Main14.java
@@ -0,0 +1,8 @@
+public class Main14 {
+    public static void main(String[] args) {
+        int [][] dias = new int [3][3];
+
+        dias [0][0] = 50;
+        System.out.println(dias [0][0]);
+        }
+}
